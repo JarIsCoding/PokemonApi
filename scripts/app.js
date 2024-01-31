1b32b5b067a5cb6479b241c5926874964cfabd11
JarIsCoding <derpyderp1098@gmail.com>
2024-01-31 08:31:32 -0800

    type images fixed

--- scripts/app.js
+++ scripts/app.js
@@ -61,14 +61,15 @@ function fillstuff(data) {
 
     //Image changer/show
 
-    let img = data.sprites.front_default;
+    let img = data.sprites.other.home.front_default;
+    console.log(img)
     PokeImg.addEventListener('click', () => {
-        if (PokeImg.src === data.sprites.front_shiny) {
-            img = data.sprites.front_default;
+        if (PokeImg.src === data.sprites.other.home.front_shiny) {
+            img = data.sprites.other.home.front_default;
             PokeImg.classList.add('border-white')
             PokeImg.classList.remove('border-x-yellow-400')
         } else {
-            img = data.sprites.front_shiny;
+            img = data.sprites.other.home.front_shiny;
             PokeImg.classList.remove('border-white')
             PokeImg.classList.add('border-x-yellow-400')
         }
@@ -106,12 +107,130 @@ function fillstuff(data) {
 
     //Type Src
 
-    let firstTypeName;
+    let TypeNames = [];
     for (let i = 0; i < data.types.length; i++) {
-        let type = data.types[i].type.name
-        console.log(type)
+        TypeNames.push(data.types[i].type.name);
     }
-
-    let type2 = data.types[1].type.name
-    console.log(type2)
+    console.log(TypeNames);
+    
+    switch (TypeNames[0]) {
+        case "fire":
+            TypeImg.src = 'assets/Fire.png';
+            break;
+        case "water":
+            TypeImg.src = "assets/Water.png";
+            break;
+        case "grass":
+            TypeImg.src = "assets/Grass.png";
+            break;
+        case "ground":
+            TypeImg.src = "assets/Ground.png";
+            break;
+        case "rock":
+            TypeImg.src = "assets/Rock.png";
+            break;
+        case "normal":
+            TypeImg.src = "assets/Normal.png";
+            break;
+        case "steel":
+            TypeImg.src = "assets/Steel.png";
+            break;
+        case "flying":
+            TypeImg.src = "assets/Fly.png";
+            break;
+        case "ghost":
+            TypeImg.src = "assets/Ghost.png";
+            break;
+        case "poison":
+            TypeImg.src = "assets/Posion.png";
+            break;
+        case "electric":
+            TypeImg.src = "assets/Eletric.png";
+            break;
+        case "ice":
+            TypeImg.src = "assets/Ice.png";
+            break;
+        case "fairy":
+            TypeImg.src = "assets/Fairy.png";
+            break;
+        case "psychic":
+            TypeImg.src = "assets/Psy.png";
+            break;
+        case "bug":
+            TypeImg.src = "assets/Bug.png";
+            break;
+        case "dark":
+            TypeImg.src = "assets/Dark.png";
+            break;
+        case "fighting":
+            TypeImg.src = "assets/Fight.png";
+            break;
+        case "dragon":
+            TypeImg.src = "assets/Dragon.png";
+            break;
+        default:
+            TypeImg.src = "";
+            console.log("No Pokemon Found");
+            break;
+    }
+    switch (TypeNames[1]) {
+        case "fire":
+            TypeImg2.src = 'assets/Fire.png';
+            break;
+        case "water":
+            TypeImg2.src = "assets/Water.png";
+            break;
+        case "grass":
+            TypeImg2.src = "assets/Grass.png";
+            break;
+        case "ground":
+            TypeImg2.src = "assets/Ground.png";
+            break;
+        case "rock":
+            TypeImg2.src = "assets/Rock.png";
+            break;
+        case "normal":
+            TypeImg2.src = "assets/Normal.png";
+            break;
+        case "steel":
+            TypeImg2.src = "assets/Steel.png";
+            break;
+        case "flying":
+            TypeImg2.src = "assets/Fly.png";
+            break;
+        case "ghost":
+            TypeImg2.src = "assets/Ghost.png";
+            break;
+        case "poison":
+            TypeImg2.src = "assets/Posion.png";
+            break;
+        case "electric":
+            TypeImg2.src = "assets/Eletric.png";
+            break;
+        case "ice":
+            TypeImg2.src = "assets/Ice.png";
+            break;
+        case "fairy":
+            TypeImg2.src = "assets/Fairy.png";
+            break;
+        case "psychic":
+            TypeImg2.src = "assets/Psy.png";
+            break;
+        case "bug":
+            TypeImg2.src = "assets/Bug.png";
+            break;
+        case "dark":
+            TypeImg2.src = "assets/Dark.png";
+            break;
+        case "fighting":
+            TypeImg2.src = "assets/Fight.png";
+            break;
+        case "dragon":
+            TypeImg2.src = "assets/Dragon.png";
+            break;
+        default:
+            TypeImg2.src = "";
+            console.log("No Second Type");
+            break;
+    }
 }
